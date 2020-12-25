2f6c951949358906d135cc9ee9c30e20d4a5055c
yawar2019 <yawarali17@gmail.com>
2020-12-25 10:31:16 +0530

    Updated Code

---
+++ MyFirstApplication/LoopingExample.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApplication
+{
+    class LoopingExample
+    {
+        public static void Main() {
+            LoopingExample obj = new LoopingExample();
+            // obj. Print5TimesName();
+            //obj.PrintSumOfNumbers();
+            //obj.PrintReverseNumber();
+            obj.PrintYourTable();
+            Console.ReadLine();
+        }
+
+        void Print5TimesName()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Fahmeeda"+i);
+
+            }
+        }
+
+
+        void PrintReverseNumber()
+        {
+            for (int i = 5; i >=1; i--)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+        void PrintSumOfNumbers()
+        {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                  sum = sum + i;
+            }
+            Console.WriteLine(sum);
+
+        }
+
+        void PrintYourTable()
+        {
+            Console.WriteLine("Please Enter Table you want to Learn");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+
+                Console.WriteLine("{0}x{1}={2}",n,i,n*i);
+
+
+            }
+        }
+    }
+}
